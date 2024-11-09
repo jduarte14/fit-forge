a0508ed124a2f38167735504c7dc2b442b60b248
jduarte14 <81990291+jduarte14@users.noreply.github.com>
2024-11-09 01:28:32 -0300

    uploads profile things

--- src/app/components/home/HomeProfile.jsx
+++ src/app/components/home/HomeProfile.jsx
@@ -1,16 +1,16 @@
-//import { Octicons } from '@expo/vector-icons';
+import { useUser } from "./../../contexts/user/UserContext";
 import { View, StyleSheet, Text, Image, Dimensions, TouchableOpacity} from 'react-native';
 import { Octicons } from '@expo/vector-icons';
 const screenWidth = Dimensions.get('window').width;
 
 const HomeProfile = () => {
-
+    const { user } = useUser();
     return (
         <View style={styles.container}>
             <View style={styles.rowContainer}>
                 <View style={styles.row}>
                     <View style={styles.col}>
-                        <Text style={styles.title}>Hi Joaco!</Text>
+                        <Text style={styles.title}>Hi {user.username}!</Text>
                         <Text style={styles.subTitle}>Good morning</Text>
                     </View>
                 </View>
@@ -20,7 +20,7 @@ const HomeProfile = () => {
                     </TouchableOpacity>
                     <Image
                         style={styles.profileImage}
-                        source={require('./../../../img/avatar.png')}
+                        source={user.avatar ? { uri: user.avatar } : require('./../../../img/avatar.png')}
                     />
                 </View>
             </View>
@@ -56,11 +56,14 @@ const styles = StyleSheet.create({
     col: {
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "flex-start",
     },
     profileImage: {
-        width: 55,
-        height: 55,
+        width: 65,
+        height: 65,
+        borderRadius: 100,
+        borderColor: "#51565b",
+        borderWidth: 2,
     },
     icon: {
         marginRight: 20,
